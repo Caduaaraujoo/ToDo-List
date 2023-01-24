524c2bb0b32101a726f2076d0ce4afafe8e55f0e
Caduaaraujoo <caduaaraujoo@gmail.com>
2023-01-24 20:42:23 -0300

    Iniciando Projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './global.css'
+import style from './App.module.css'
+import { Header } from './Components/Header'
+import { Main } from './Components/Main'
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  )
+}
